345c58e49d38ab0e0f212137a522454d56019e50
Ricardo Rodrigues <93055503+rrccdd6@users.noreply.github.com>
2023-02-03 18:40:28 -0300

    Versão 1.1
    Passei a validação pelo Chat GPT

--- js/login.js
+++ js/login.js
@@ -1,39 +1,61 @@
 // VARIÁVEIS
 
-var usuario = document.getElementById("usuario").value;
-var senha = document.getElementById("senha").value;
-var login = $("input:usuario")
-var psw = $("input:senha")
+//var usuario = document.getElementById("usuario").value;
+//var senha = document.getElementById("senha").value;
+//var login = $("input:usuario")
+//var psw = $("input:senha")
 
 // FUNÇAO SUBMIT
 
-function submit_by_id() {
+//function submit_by_id() {
 
-    var usuario = document.getElementById("usuario").value;
-    var senha = document.getElementById("senha").value;
+   // var usuario = document.getElementById("usuario").value;
+   // var senha = document.getElementById("senha").value;
 
-    if (validation(true)) // CHAMADA DE VALIDAÇAO
-    {
-        document.getElementById("formulario").submit();
-        alert(" Usuário : " + usuario + " Senha : " + senha + " " + " ENTRADA AUTORIZADA");
-    }
-}
-
-
-
+   // if (validation(true)) // CHAMADA DE VALIDAÇAO
+   // {
+  //      document.getElementById("formulario").submit();
+   //     alert(" Usuário : " + usuario + " Senha : " + senha + " " + " ENTRADA AUTORIZADA");
+  //  }
+//}
 
 // VALIDAÇÃO DE USUARIO E SENHA 
 
-function validation() {
+//function validation() {
 
-    var usuario = document.getElementById("usuario").value;
-    var senha = document.getElementById("senha").value;
+ //   var usuario = document.getElementById("usuario").value;
+//var senha = document.getElementById("senha").value;
 
-    if (usuario === '' || senha === '') {
-        alert("ACESSO NEGADO");
-        return false;
-    } else {
-        window.localStorage
-        return true;
-    }
-}
+//    if (usuario === '' || senha === '') {
+ //       alert("ACESSO NEGADO");
+ //       return false;
+ //   } else {
+  //      window.localStorage
+ //       return true;
+ //   }
+//}
+
+   // VARIÁVEIS
+   var username = "";
+   var password = "";
+
+   // FUNÇAO SUBMIT
+   function submitForm() {
+     username = document.getElementById("usuario").value;
+     password = document.getElementById("senha").value;
+
+     if (validateCredentials()) {
+       document.getElementById("formulario").submit();
+       alert(" Usuário: " + username + " Senha: " + password + " ENTRADA AUTORIZADA");
+     }
+   }
+
+   // VALIDAÇÃO DE USUARIO E SENHA 
+   function validateCredentials() {
+     if (username === '' || password === '') {
+       alert("ACESSO NEGADO. Por favor, preencha todos os campos.");
+       return false;
+     } else {
+       return true;
+     }
+   }
